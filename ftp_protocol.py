f96b724bfc028ee0c47c0bcaf20af13ab14a056d
melissaliu07 <157554008+melissaliu07@users.noreply.github.com>
2024-11-27 04:04:57 -0800

    Update ftp_protocol.py

--- ftp_protocol.py
+++ ftp_protocol.py
@@ -1,1 +1,16 @@
+import socket
 
+# Command codes
+CMD_LIST = "LIST"
+CMD_GET = "GET"
+CMD_PUT = "PUT"
+CMD_PORT = "PORT"
+CMD_QUIT = "QUIT"
+
+# Buffer sizes
+HEADER_SIZE = 10
+BUFFER_SIZE = 65536
+
+# Response codes
+RESP_OK = "200"
+RESP_ERROR = "500"
